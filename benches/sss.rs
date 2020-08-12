81afd0b9ffe03dc3cc514c76c6eb80d71a950f93
ross <ross.pure@gmail.com>
2020-08-12 12:37:44 +1000

    initial sss impl and tests

---
+++ benches/sss.rs
@@ -0,0 +1,33 @@
+#![feature(test)]
+
+extern crate test;
+
+use secp256k1::scalar::{self, Scalar};
+use shamir::{self, Share};
+use test::Bencher;
+
+#[bench]
+fn bench_share_secret(b: &mut Bencher) {
+    const N: usize = 100;
+    let k = 33;
+
+    let mut indices = [Scalar::default(); N];
+    scalar::randomise_scalars_using_thread_rng(&mut indices);
+    let mut shares = [Share::default(); N];
+    let secret = Scalar::new_random_using_thread_rng();
+    b.iter(|| shamir::share_secret_in_place(&mut shares, &indices, &secret, k));
+}
+
+#[bench]
+fn bench_reconstruct_secret(b: &mut Bencher) {
+    const N: usize = 100;
+    let k = 33;
+
+    let mut indices = [Scalar::default(); N];
+    scalar::randomise_scalars_using_thread_rng(&mut indices);
+    let mut shares = [Share::default(); N];
+    let secret = Scalar::new_random_using_thread_rng();
+    shamir::share_secret_in_place(&mut shares, &indices, &secret, k);
+    let mut reconstructed = Scalar::default();
+    b.iter(|| shamir::interpolate_shares_at_zero_in_place(&mut reconstructed, &shares[..k]));
+}
